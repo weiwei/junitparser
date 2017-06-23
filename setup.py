22927994753615af5d4f4f32c77e92bf3117adf4
Joel Wang <joel.wang@autodesk.com>
2017-06-23 15:07:05 +0800

    revert to 0.9.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
         return ''
 
 setup(name='junitparser',
-      version='0.9.1',
+      version='0.9.0',
       description='Manipulates JUnit/xUnit Result XML files',
       long_description=read('README.rst'),
       classifiers=[
