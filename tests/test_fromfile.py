f0461e3c6632acc04fd361e8a7d0b35f53d60b2f
Weiwei <gastlygem@gmail.com>
2023-05-07 13:00:12 +0800

    use pytest

--- tests/test_fromfile.py
+++ tests/test_fromfile.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-import unittest
+import pytest
 from junitparser import (
     TestCase,
     TestSuite,
@@ -19,175 +19,171 @@ from junitparser import (
 
 try:
     from lxml.etree import XMLParser, parse
-
     has_lxml = True
 except ImportError:
     has_lxml = False
 
-from io import open
 
+@pytest.fixture(scope="module")
+def tmpfile():
+    import tempfile
 
-class Test_RealFile(unittest.TestCase):
-    def setUp(self):
-        import tempfile
+    tmp = tempfile.mktemp(suffix=".xml")
+    yield tmp
+    if os.path.exists(tmp):
+        os.remove(tmp)
 
-        self.tmp = tempfile.mktemp(suffix=".xml")
+def test_fromfile(tmpfile):
+    xml = JUnitXml.fromfile(
+        os.path.join(os.path.dirname(__file__), "data/normal.xml")
+    )
+    suite1, suite2 = list(iter(xml))
+    assert len(list(suite1.properties())) == 0
+    assert len(list(suite2.properties())) == 3
+    assert len(suite2) == 3
+    assert suite2.name == "JUnitXmlReporter.constructor"
+    assert suite2.tests == 3
+    cases = list(suite2.iterchildren(TestCase))
+    assert isinstance(cases[0].result[0], Failure)
+    assert isinstance(cases[1].result[0], Skipped)
+    assert len(cases[2].result) == 0
 
-    def tearDown(self):
-        if os.path.exists(self.tmp):
-            os.remove(self.tmp)
+@pytest.mark.skipif(not has_lxml, reason="lxml required to run the case")
+def test_fromfile_with_parser(tmpfile):
+    def parse_func(file_path):
+        xml_parser = XMLParser(huge_tree=True)
+        return parse(file_path, xml_parser)
 
-    def test_fromfile(self):
-        xml = JUnitXml.fromfile(
-            os.path.join(os.path.dirname(__file__), "data/normal.xml")
-        )
-        suite1, suite2 = list(iter(xml))
-        self.assertEqual(len(list(suite1.properties())), 0)
-        self.assertEqual(len(list(suite2.properties())), 3)
-        self.assertEqual(len(suite2), 3)
-        self.assertEqual(suite2.name, "JUnitXmlReporter.constructor")
-        self.assertEqual(suite2.tests, 3)
-        cases = list(suite2.iterchildren(TestCase))
-        self.assertIsInstance(cases[0].result[0], Failure)
-        self.assertIsInstance(cases[1].result[0], Skipped)
-        self.assertEqual(len(cases[2].result), 0)
+    xml = JUnitXml.fromfile(
+        os.path.join(os.path.dirname(__file__), "data/normal.xml"),
+        parse_func=parse_func,
+    )
+    suite1, suite2 = list(iter(xml))
+    assert len(list(suite1.properties())) == 0
+    assert len(list(suite2.properties())) == 3
+    assert len(suite2) == 3
+    assert suite2.name == "JUnitXmlReporter.constructor"
+    assert suite2.tests == 3
+    cases = list(suite2.iterchildren(TestCase))
+    assert isinstance(cases[0].result[0], Failure)
+    assert isinstance(cases[1].result[0], Skipped)
+    assert len(cases[2].result) == 0
 
-    @unittest.skipUnless(has_lxml, "lxml required to run the case")
-    def test_fromfile_with_parser(self):
-        def parse_func(file_path):
-            xml_parser = XMLParser(huge_tree=True)
-            return parse(file_path, xml_parser)
+def test_fromfile_without_testsuites_tag(tmpfile):
+    xml = JUnitXml.fromfile(
+        os.path.join(os.path.dirname(__file__), "data/no_suites_tag.xml")
+    )
+    cases = list(iter(xml))
+    properties = list(iter(xml.properties()))
+    assert len(properties) == 3
+    assert len(cases) == 3
+    assert xml.name == "JUnitXmlReporter.constructor"
+    assert xml.tests == 3
+    assert isinstance(cases[0].result[0], Failure)
+    assert isinstance(cases[1].result[0], Skipped)
+    assert len(cases[2].result) == 0
 
-        xml = JUnitXml.fromfile(
-            os.path.join(os.path.dirname(__file__), "data/normal.xml"),
-            parse_func=parse_func,
-        )
-        suite1, suite2 = list(iter(xml))
-        self.assertEqual(len(list(suite1.properties())), 0)
-        self.assertEqual(len(list(suite2.properties())), 3)
-        self.assertEqual(len(suite2), 3)
-        self.assertEqual(suite2.name, "JUnitXmlReporter.constructor")
-        self.assertEqual(suite2.tests, 3)
-        cases = list(suite2.iterchildren(TestCase))
-        self.assertIsInstance(cases[0].result[0], Failure)
-        self.assertIsInstance(cases[1].result[0], Skipped)
-        self.assertEqual(len(cases[2].result), 0)
+def test_fromfile_with_testsuite_in_testsuite(tmpfile):
+    xml = JUnitXml.fromfile(
+        os.path.join(os.path.dirname(__file__), "data/jenkins.xml")
+    )
+    suite1, suite2 = list(iter(xml))
+    assert len(list(suite1.properties())) == 0
+    assert len(list(suite2.properties())) == 3
+    assert len(suite2) == 3
+    assert suite2.name == "JUnitXmlReporter.constructor"
+    assert suite2.tests == 3
+    direct_cases = list(suite2.iterchildren(TestCase))
+    assert len(direct_cases) == 1
+    assert isinstance(direct_cases[0].result[0], Failure)
+    all_cases = list(suite2)
+    assert isinstance(all_cases[0].result[0], Failure)
+    assert isinstance(all_cases[1].result[0], Skipped)
+    assert len(all_cases[2].result) == 0
 
-    def test_fromfile_without_testsuites_tag(self):
-        xml = JUnitXml.fromfile(
-            os.path.join(os.path.dirname(__file__), "data/no_suites_tag.xml")
-        )
-        cases = list(iter(xml))
-        properties = list(iter(xml.properties()))
-        self.assertEqual(len(properties), 3)
-        self.assertEqual(len(cases), 3)
-        self.assertEqual(xml.name, "JUnitXmlReporter.constructor")
-        self.assertEqual(xml.tests, 3)
-        self.assertIsInstance(cases[0].result[0], Failure)
-        self.assertIsInstance(cases[1].result[0], Skipped)
-        self.assertEqual(len(cases[2].result), 0)
+def test_write_xml_without_testsuite_tag(tmpfile):
+    suite = TestSuite()
+    suite.name = "suite1"
+    case = TestCase()
+    case.name = "case1"
+    suite.add_testcase(case)
+    suite.write(tmpfile)
+    with open(tmpfile) as f:
+        text = f.read()
+    assert "suite1" in text
+    assert "case1" in text
 
-    def test_fromfile_with_testsuite_in_testsuite(self):
-        xml = JUnitXml.fromfile(
-            os.path.join(os.path.dirname(__file__), "data/jenkins.xml")
-        )
-        suite1, suite2 = list(iter(xml))
-        self.assertEqual(len(list(suite1.properties())), 0)
-        self.assertEqual(len(list(suite2.properties())), 3)
-        self.assertEqual(len(suite2), 3)
-        self.assertEqual(suite2.name, "JUnitXmlReporter.constructor")
-        self.assertEqual(suite2.tests, 3)
-        direct_cases = list(suite2.iterchildren(TestCase))
-        self.assertEqual(len(direct_cases), 1)
-        self.assertIsInstance(direct_cases[0].result[0], Failure)
-        all_cases = list(suite2)
-        self.assertIsInstance(all_cases[0].result[0], Failure)
-        self.assertIsInstance(all_cases[1].result[0], Skipped)
-        self.assertEqual(len(all_cases[2].result), 0)
+def test_file_is_not_xml(tmpfile):
+    text = "Not really an xml file"
+    with open(tmpfile, "w") as f:
+        f.write(text)
+    with pytest.raises(Exception):
+        xml = JUnitXml.fromfile(tmpfile)
+        # Raises lxml.etree.XMLSyntaxError
 
-    def test_write_xml_withouth_testsuite_tag(self):
-        suite = TestSuite()
-        suite.name = "suite1"
-        case = TestCase()
-        case.name = "case1"
-        suite.add_testcase(case)
-        suite.write(self.tmp)
-        with open(self.tmp) as f:
-            text = f.read()
-        self.assertIn("suite1", text)
-        self.assertIn("case1", text)
+def test_illegal_xml_file(tmpfile):
+    text = "<some></some>"
+    with open(tmpfile, "w") as f:
+        f.write(text)
+    with pytest.raises(JUnitXmlError):
+        xml = JUnitXml.fromfile(tmpfile)
 
-    def test_file_is_not_xml(self):
-        text = "Not really an xml file"
-        with open(self.tmp, "w") as f:
-            f.write(text)
-        with self.assertRaises(Exception):
-            xml = JUnitXml.fromfile(self.tmp)
-            # Raises lxml.etree.XMLSyntaxError
+def test_write(tmpfile):
+    suite1 = TestSuite()
+    suite1.name = "suite1"
+    case1 = TestCase()
+    case1.name = "case1"
+    suite1.add_testcase(case1)
+    result = JUnitXml()
+    result.add_testsuite(suite1)
+    result.write(tmpfile)
+    with open(tmpfile) as f:
+        text = f.read()
+    assert "suite1" in text
+    assert "case1" in text
 
-    def test_illegal_xml_file(self):
-        text = "<some></some>"
-        with open(self.tmp, "w") as f:
-            f.write(text)
-        with self.assertRaises(JUnitXmlError):
-            xml = JUnitXml.fromfile(self.tmp)
+def test_write_noarg(tmpfile):
+    suite1 = TestSuite()
+    suite1.name = "suite1"
+    case1 = TestCase()
+    case1.name = "case1"
+    suite1.add_testcase(case1)
+    result = JUnitXml()
+    result.add_testsuite(suite1)
+    with pytest.raises(JUnitXmlError):
+        result.write()
 
-    def test_write(self):
-        suite1 = TestSuite()
-        suite1.name = "suite1"
-        case1 = TestCase()
-        case1.name = "case1"
-        suite1.add_testcase(case1)
-        result = JUnitXml()
-        result.add_testsuite(suite1)
-        result.write(self.tmp)
-        with open(self.tmp) as f:
-            text = f.read()
-        self.assertIn("suite1", text)
-        self.assertIn("case1", text)
+def test_write_nonascii(tmpfile):
+    suite1 = TestSuite()
+    suite1.name = "suite1"
+    case1 = TestCase()
+    case1.name = "用例1"
+    suite1.add_testcase(case1)
+    result = JUnitXml()
+    result.add_testsuite(suite1)
+    result.write(tmpfile)
+    with open(tmpfile, encoding="utf-8") as f:
+        text = f.read()
+    assert "suite1" in text
+    assert "用例1" in text
 
-    def test_write_noarg(self):
-        suite1 = TestSuite()
-        suite1.name = "suite1"
-        case1 = TestCase()
-        case1.name = "case1"
-        suite1.add_testcase(case1)
-        result = JUnitXml()
-        result.add_testsuite(suite1)
-        with self.assertRaises(JUnitXmlError):
-            result.write()
+def test_read_written_xml(tmpfile):
+    suite1 = TestSuite()
+    suite1.name = "suite1"
+    case1 = TestCase()
+    case1.name = "用例1"
+    suite1.add_testcase(case1)
+    result = JUnitXml()
+    result.add_testsuite(suite1)
+    result.write(tmpfile)
+    xml = JUnitXml.fromfile(tmpfile)
+    suite = next(iter(xml))
+    case = next(iter(suite))
+    assert case.name == "用例1"
 
-    def test_write_nonascii(self):
-        suite1 = TestSuite()
-        suite1.name = "suite1"
-        case1 = TestCase()
-        case1.name = "用例1"
-        suite1.add_testcase(case1)
-        result = JUnitXml()
-        result.add_testsuite(suite1)
-        result.write(self.tmp)
-        with open(self.tmp, encoding="utf-8") as f:
-            text = f.read()
-        self.assertIn("suite1", text)
-        self.assertIn("用例1", text)
-
-    def test_read_written_xml(self):
-        suite1 = TestSuite()
-        suite1.name = "suite1"
-        case1 = TestCase()
-        case1.name = "用例1"
-        suite1.add_testcase(case1)
-        result = JUnitXml()
-        result.add_testsuite(suite1)
-        result.write(self.tmp)
-        xml = JUnitXml.fromfile(self.tmp)
-        suite = next(iter(xml))
-        case = next(iter(suite))
-        self.assertEqual(case.name, "用例1")
-
-    def test_multi_results_in_case(self):
-        # Has to be a binary string to include xml declarations.
-        text = b"""<?xml version="1.0" encoding="UTF-8"?>
+def test_multi_results_in_case(tmpfile):
+    # Has to be a binary string to include xml declarations.
+    text = b"""<?xml version="1.0" encoding="UTF-8"?>
 <testsuites>
    <testsuite name="JUnitXmlReporter.constructor">
       <testcase classname="JUnitXmlReporter.constructor" name="should default path to an empty string" time="0.006">
@@ -196,21 +192,21 @@ class Test_RealFile(unittest.TestCase):
       </testcase>
    </testsuite>
 </testsuites>"""
-        xml = JUnitXml.fromstring(text)
-        suite = next(iter(xml))
-        case = next(iter(suite))
-        self.assertEqual(len(case.result), 2)
+    xml = JUnitXml.fromstring(text)
+    suite = next(iter(xml))
+    case = next(iter(suite))
+    assert len(case.result) == 2
 
-    def test_write_pretty(self):
-        suite1 = TestSuite()
-        suite1.name = "suite1"
-        case1 = TestCase()
-        case1.name = "用例1"
-        suite1.add_testcase(case1)
-        result = JUnitXml()
-        result.add_testsuite(suite1)
-        result.write(self.tmp, pretty=True)
-        xml = JUnitXml.fromfile(self.tmp)
-        suite = next(iter(xml))
-        case = next(iter(suite))
-        self.assertEqual(case.name, "用例1")
+def test_write_pretty(tmpfile):
+    suite1 = TestSuite()
+    suite1.name = "suite1"
+    case1 = TestCase()
+    case1.name = "用例1"
+    suite1.add_testcase(case1)
+    result = JUnitXml()
+    result.add_testsuite(suite1)
+    result.write(tmpfile, pretty=True)
+    xml = JUnitXml.fromfile(tmpfile)
+    suite = next(iter(xml))
+    case = next(iter(suite))
+    assert case.name == "用例1"
--- tests/test_xunit2.py
+++ tests/test_xunit2.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 
-import unittest
 from junitparser.xunit2 import JUnitXml, TestSuite, TestCase, RerunFailure
 from junitparser import Failure
 from copy import deepcopy
 
 
-class Test_TestCase(unittest.TestCase):
+class Test_TestCase:
     def test_case_fromstring(self):
         text = """<testcase name="testname">
         <failure message="failure message" type="FailureType"/>
@@ -21,83 +20,86 @@ class Test_TestCase(unittest.TestCase):
         <system-err>System err</system-err>
         </testcase>"""
         case = TestCase.fromstring(text)
-        self.assertEqual(case.name, "testname")
-        self.assertIsInstance(case.result[0], Failure)
-        self.assertEqual(case.system_out, "System out")
-        self.assertEqual(case.system_err, "System err")
+        assert case.name == "testname"
+        assert isinstance(case.result[0], Failure)
+        assert case.system_out == "System out"
+        assert case.system_err == "System err"
         rerun_failures = case.rerun_failures()
-        self.assertEqual(len(rerun_failures), 2)
-        self.assertEqual(rerun_failures[0].message, "Not found")
-        self.assertEqual(rerun_failures[0].stack_trace, None)
-        self.assertEqual(rerun_failures[0].system_out, "No ha encontrado")
-        self.assertEqual(rerun_failures[1].stack_trace, "Stacktrace")
-        self.assertEqual(rerun_failures[1].system_err, "Error del servidor")
-        self.assertEqual(len(case.rerun_errors()), 0)
-        self.assertEqual(len(case.flaky_failures()), 0)
-        self.assertEqual(len(case.flaky_errors()), 0)
+        assert len(rerun_failures) == 2
+        assert rerun_failures[0].message == "Not found"
+        assert rerun_failures[0].stack_trace is None
+        assert rerun_failures[0].system_out == "No ha encontrado"
+        assert rerun_failures[0].system_err is None
+        assert rerun_failures[1].message == "Server error"
+        assert rerun_failures[1].stack_trace == "Stacktrace"
+        assert rerun_failures[1].system_out is None
+        assert rerun_failures[1].system_err == "Error del servidor"
+        assert len(case.rerun_errors()) == 0
+        assert len(case.flaky_failures()) == 0
+        assert len(case.flaky_errors()) == 0
 
-    def test_rerun(self):
+    def test_case_rerun(self):
         case = TestCase("testname")
         rerun_failure = RerunFailure("Not found", "404")
-        self.assertEqual(rerun_failure.system_out, None)
-        self.assertEqual(rerun_failure.system_err, None)
+        assert rerun_failure.system_out == None
+        assert rerun_failure.system_err == None
         rerun_failure.stack_trace = "Stack"
         rerun_failure.system_err = "E404"
         rerun_failure.system_out = "NOT FOUND"
         case.add_rerun_result(rerun_failure)
-        self.assertEqual(len(case.rerun_failures()), 1)
+        assert len(case.rerun_failures()) == 1
         # Interesting, same object is only added once by xml libs
         failure2 = deepcopy(rerun_failure)
         failure2.stack_trace = "Stack2"
         failure2.system_err = "E401"
         failure2.system_out = "401 Error"
         case.add_rerun_result(failure2)
-        self.assertEqual(len(case.rerun_failures()), 2)
+        assert len(case.rerun_failures()) == 2
 
 
-class Test_TestSuite(unittest.TestCase):
+class Test_TestSuite:
     def test_properties(self):
         suite = TestSuite("mySuite")
-        self.assertEqual(suite.system_out, None)
-        self.assertEqual(suite.system_err, None)
+        assert suite.system_out is None
+        assert suite.system_err is None
         suite.system_err = "System err"
         suite.system_out = "System out"
-        self.assertEqual(suite.system_out, "System out")
-        self.assertEqual(suite.system_err, "System err")
+        assert suite.system_out == "System out"
+        assert suite.system_err == "System err"
         suite.system_err = "System err2"
         suite.system_out = "System out2"
-        self.assertEqual(suite.system_out, "System out2")
-        self.assertEqual(suite.system_err, "System err2")
+        assert suite.system_out == "System out2"
+        assert suite.system_err == "System err2"
+
 
     def test_iterate_case(self):
         suite = TestSuite("mySuite")
         suite.add_testcase(TestCase("test1"))
         case = next(iter(suite))
-        self.assertEqual(case.name, "test1")
+        assert case.name == "test1"
 
     def test_iterate_suite(self):
         suite = TestSuite("mySuite")
         suite.add_testsuite(TestSuite("suite1"))
         suite = next(suite.testsuites())
-        self.assertEqual(suite.name, "suite1")
+        assert suite.name == "suite1"
 
     def test_remove_case(self):
         suite = TestSuite("mySuite")
         test = TestCase("test1")
         suite.add_testcase(test)
         suite.remove_testcase(test)
-        self.assertEqual(list(iter(suite)), [])
+        assert len(suite) == 0
 
 
-class Test_JUnitXml(unittest.TestCase):
+class Test_JUnitXml:
     def test_init(self):
         xml = JUnitXml("tests")
-        self.assertEqual(xml.name, "tests")
+        assert xml.name == "tests"
         xml = JUnitXml("myname")
         xml.add_testsuite(TestSuite("suite1"))
         xml.update_statistics()
-        self.assertEqual(xml.skipped, None)
-        # errors="0"
-        self.assertEqual(xml.tostring().count(b"errors"), 2)
-        # "skipped" attribute doesn't exist
-        self.assertEqual(xml.tostring().count(b"skipped"), 1)
+        assert xml.skipped is None
+        assert xml.tostring().count(b"errors") == 2
+        assert xml.tostring().count(b"skipped") == 1
+
