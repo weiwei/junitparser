2c0ee7b5030d7727407a25bcfd26ebb8a0701403
Enrico Minack <github@enrico.minack.dev>
2025-01-13 11:24:54 +0100

    Add more is_* properties to JUnit and XUnit2 TestCase
    - Add is_failure and is_error to JUnit TestCase
    - Add is_rerun and is_flaky to XUnit2 TestCase
    - Provide access to XUnit2 interim (rerun & flaky) results
    
    Co-authored-by: Jan Wille <mail@janwille.de>

--- tests/test_xunit2.py
+++ tests/test_xunit2.py
@@ -1,10 +1,10 @@
-from junitparser.xunit2 import JUnitXml, TestSuite, TestCase, RerunFailure
+from junitparser.xunit2 import JUnitXml, TestSuite, TestCase, RerunFailure, RerunError, FlakyFailure, FlakyError
 from junitparser import Failure
 from copy import deepcopy
 
 
 class Test_TestCase:
-    def test_case_fromstring(self):
+    def test_case_rerun_fromstring(self):
         text = """<testcase name="testname">
         <failure message="failure message" type="FailureType"/>
         <rerunFailure message="Not found" type="404">
@@ -14,28 +14,110 @@ class Test_TestCase:
             <system-err>Error del servidor</system-err>
             <stackTrace>Stacktrace</stackTrace>
         </rerunFailure>
+        <rerunError message="Setup error"/>
         <system-out>System out</system-out>
         <system-err>System err</system-err>
         </testcase>"""
         case = TestCase.fromstring(text)
         assert isinstance(case, TestCase)
         assert case.name == "testname"
+        assert len(case.result) == 1
         assert isinstance(case.result[0], Failure)
         assert case.system_out == "System out"
         assert case.system_err == "System err"
+        assert case.is_passed == False
+        assert case.is_failure == True
+        assert case.is_error == False
+        assert case.is_skipped == False
+        assert case.is_rerun == True
+        assert case.is_flaky == False
+
+        interim_results = case.interim_result
+        assert len(interim_results) == 3
+        assert isinstance(interim_results[0], RerunFailure)
+        assert isinstance(interim_results[1], RerunFailure)
+        assert isinstance(interim_results[2], RerunError)
+
         rerun_failures = case.rerun_failures()
         assert len(rerun_failures) == 2
+        assert isinstance(rerun_failures[0], RerunFailure)
         assert rerun_failures[0].message == "Not found"
         assert rerun_failures[0].stack_trace is None
         assert rerun_failures[0].system_out == "No ha encontrado"
         assert rerun_failures[0].system_err is None
+        assert isinstance(rerun_failures[1], RerunFailure)
         assert rerun_failures[1].message == "Server error"
         assert rerun_failures[1].stack_trace == "Stacktrace"
         assert rerun_failures[1].system_out is None
         assert rerun_failures[1].system_err == "Error del servidor"
-        assert len(case.rerun_errors()) == 0
+
+        rerun_errors = case.rerun_errors()
+        assert len(rerun_errors) == 1
+        assert isinstance(rerun_errors[0], RerunError)
+        assert rerun_errors[0].message == "Setup error"
+        assert rerun_errors[0].stack_trace is None
+        assert rerun_errors[0].system_out is None
+        assert rerun_errors[0].system_err is None
+
         assert len(case.flaky_failures()) == 0
+
         assert len(case.flaky_errors()) == 0
+
+    def test_case_flaky_fromstring(self):
+        text = """<testcase name="testname">
+        <flakyFailure message="Not found" type="404">
+            <system-out>No ha encontrado</system-out>
+        </flakyFailure>
+        <flakyFailure message="Server error" type="500">
+            <system-err>Error del servidor</system-err>
+            <stackTrace>Stacktrace</stackTrace>
+        </flakyFailure>
+        <flakyError message="Setup error"/>
+        <system-out>System out</system-out>
+        <system-err>System err</system-err>
+        </testcase>"""
+        case = TestCase.fromstring(text)
+        assert case.name == "testname"
+        assert len(case.result) == 0
+        assert case.system_out == "System out"
+        assert case.system_err == "System err"
+        assert case.is_passed == True
+        assert case.is_failure == False
+        assert case.is_error == False
+        assert case.is_skipped == False
+        assert case.is_rerun == False
+        assert case.is_flaky == True
+
+        interim_results = case.interim_result
+        assert len(interim_results) == 3
+        assert isinstance(interim_results[0], FlakyFailure)
+        assert isinstance(interim_results[1], FlakyFailure)
+        assert isinstance(interim_results[2], FlakyError)
+
+        assert len(case.rerun_failures()) == 0
+
+        assert len(case.rerun_errors()) == 0
+
+        flaky_failures = case.flaky_failures()
+        assert len(flaky_failures) == 2
+        assert isinstance(flaky_failures[0], FlakyFailure)
+        assert flaky_failures[0].message == "Not found"
+        assert flaky_failures[0].stack_trace is None
+        assert flaky_failures[0].system_out == "No ha encontrado"
+        assert flaky_failures[0].system_err is None
+        assert isinstance(flaky_failures[1], FlakyFailure)
+        assert flaky_failures[1].message == "Server error"
+        assert flaky_failures[1].stack_trace == "Stacktrace"
+        assert flaky_failures[1].system_out is None
+        assert flaky_failures[1].system_err == "Error del servidor"
+
+        flaky_errors = case.flaky_errors()
+        assert len(flaky_errors) == 1
+        assert isinstance(flaky_errors[0], FlakyError)
+        assert flaky_errors[0].message == "Setup error"
+        assert flaky_errors[0].stack_trace is None
+        assert flaky_errors[0].system_out is None
+        assert flaky_errors[0].system_err is None
 
     def test_case_rerun(self):
         case = TestCase("testname")
