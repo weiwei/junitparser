e12aef9077dd14716ba23d2d391e56ee0f4c1353
Jan Wille <mail@janwille.de>
2025-01-14 21:07:37 +0100

    test: import `cli` module to avoid to many imports

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import pytest
-from junitparser.cli import verify, merge
+from junitparser import cli
 
 DATA_DIR = Path(__file__).parent / "data"
 
@@ -11,14 +11,14 @@ DATA_DIR = Path(__file__).parent / "data"
 )
 def test_verify(file: str, expected_exitcode: int):
     path = DATA_DIR / file
-    assert verify([path]) == expected_exitcode
+    assert cli.verify([path]) == expected_exitcode
 
 
 def test_merge(tmp_path: Path):
     files = [DATA_DIR / "jenkins.xml", DATA_DIR / "pytest_success.xml"]
     suites = ["JUnitXmlReporter", "JUnitXmlReporter.constructor", "pytest"]
     outfile = tmp_path / "merged.xml"
-    merge(files, str(outfile))
+    cli.merge(files, str(outfile))
     xml = outfile.read_text()
     for s in suites:
         assert f'testsuite name="{s}"' in xml
