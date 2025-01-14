bbfa178f7982241dcf037e9e7ae6f107e771bd51
Jan Wille <mail@janwille.de>
2025-01-14 20:42:01 +0100

    test: use pathlib in cli test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 import pytest
 from junitparser.cli import verify
 
@@ -7,6 +7,6 @@ from junitparser.cli import verify
     "file, expected_exitcode",
     [("data/jenkins.xml", 1), ("data/no_fails.xml", 0), ("data/normal.xml", 1)],
 )
-def test_verify(file, expected_exitcode):
-    path = os.path.join(os.path.dirname(__file__), file)
+def test_verify(file: str, expected_exitcode: int):
+    path = Path(__file__).parent / file
     assert verify([path]) == expected_exitcode
