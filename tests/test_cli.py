f873225e86030f606dba92d10c724b531eab21a7
Jan Wille <mail@janwille.de>
2025-01-14 20:44:34 +0100

    test: global constant for `data/`

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,11 +2,12 @@ from pathlib import Path
 import pytest
 from junitparser.cli import verify
 
+DATA_DIR = Path(__file__).parent / "data"
 
 @pytest.mark.parametrize(
     "file, expected_exitcode",
-    [("data/jenkins.xml", 1), ("data/no_fails.xml", 0), ("data/normal.xml", 1)],
+    [("jenkins.xml", 1), ("no_fails.xml", 0), ("normal.xml", 1)],
 )
 def test_verify(file: str, expected_exitcode: int):
-    path = Path(__file__).parent / file
+    path = DATA_DIR / file
     assert verify([path]) == expected_exitcode
