d4864491cf41a137b9cab4085809f025b0edd0d3
Jan Wille <mail@janwille.de>
2025-02-10 15:21:46 +0100

    test: add cli test for merging

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 import pytest
-from junitparser.cli import verify
+from junitparser.cli import verify, merge
 
 DATA_DIR = Path(__file__).parent / "data"
+
 
 @pytest.mark.parametrize(
     "file, expected_exitcode",
@@ -11,3 +12,13 @@ DATA_DIR = Path(__file__).parent / "data"
 def test_verify(file: str, expected_exitcode: int):
     path = DATA_DIR / file
     assert verify([path]) == expected_exitcode
+
+
+def test_merge(tmp_path: Path):
+    files = [DATA_DIR / "jenkins.xml", DATA_DIR / "pytest_success.xml"]
+    suites = ["JUnitXmlReporter", "JUnitXmlReporter.constructor", "pytest"]
+    outfile = tmp_path / "merged.xml"
+    merge(files, str(outfile))
+    xml = outfile.read_text()
+    for s in suites:
+        assert f'testsuite name="{s}"' in xml
